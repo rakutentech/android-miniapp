d830a5b404b70cefafcc0677ed814913fba81f14
ts-nitish-a-jain <56627842+ts-nitish-a-jain@users.noreply.github.com>
2020-02-10 17:12:18 +0900

    add few validations, update error case handling and unit tests (#34)
    Co-authored-by: Minh Nguyen (ミン） <60589925+minh-rakuten@users.noreply.github.com>

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloader.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloader.kt
@@ -1,5 +1,6 @@
 package com.rakuten.tech.mobile.miniapp
 
+import androidx.annotation.VisibleForTesting
 import com.rakuten.tech.mobile.miniapp.api.ApiClient
 import com.rakuten.tech.mobile.miniapp.api.ManifestEntity
 import com.rakuten.tech.mobile.miniapp.storage.MiniAppStorage
@@ -25,10 +26,21 @@ internal class MiniAppDownloader(
         manifest: ManifestEntity
     ): String {
         val baseSavePath = storage.getSavePathForApp(appId, versionId)
-        for (file in manifest.files) {
-            val response = apiClient.downloadFile(file)
-            storage.saveFile(file, baseSavePath, response.byteStream())
+        when {
+            // If backend functions correctly, this should never happen
+            isManifestValid(manifest) -> {
+                for (file in manifest.files) {
+                    val response = apiClient.downloadFile(file)
+                    storage.saveFile(file, baseSavePath, response.byteStream())
+                }
+                return baseSavePath
+            }
+            else -> throw MiniAppSdkException("Internal Server Error")
         }
-        return baseSavePath
     }
+
+    @Suppress("SENSELESS_COMPARISON")
+    @VisibleForTesting
+    fun isManifestValid(manifest: ManifestEntity) =
+        manifest != null && manifest.files != null && !manifest.files.isEmpty()
 }
--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloaderSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloaderSpec.kt
@@ -1,5 +1,6 @@
 package com.rakuten.tech.mobile.miniapp
 
+import com.google.gson.Gson
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
@@ -7,10 +8,13 @@ import com.rakuten.tech.mobile.miniapp.api.ApiClient
 import com.rakuten.tech.mobile.miniapp.api.ManifestEntity
 import com.rakuten.tech.mobile.miniapp.storage.MiniAppStorage
 import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody
 import org.amshove.kluent.When
 import org.amshove.kluent.calling
 import org.amshove.kluent.itReturns
 import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MiniAppDownloaderSpec {
 
@@ -20,18 +24,64 @@ class MiniAppDownloaderSpec {
             val apiClient: ApiClient = mock()
             val storage: MiniAppStorage = mock()
             val downloader = MiniAppDownloader(storage, apiClient)
+
             When calling downloader.fetchManifest(
                 TEST_ID_MINIAPP,
                 TEST_ID_MINIAPP_VERSION
-            ) itReturns ManifestEntity(listOf())
+            ) itReturns ManifestEntity(listOf(TEST_URL_HTTPS_1))
 
-            downloader.startDownload(
-                TEST_ID_MINIAPP,
-                TEST_ID_MINIAPP_VERSION
-            )
+            val mockResponseBody = ResponseBody.create(null, TEST_BODY_CONTENT)
+            When calling apiClient.downloadFile(TEST_URL_HTTPS_1) itReturns mockResponseBody
+            downloader.startDownload(TEST_ID_MINIAPP, TEST_ID_MINIAPP_VERSION)
+
             verify(apiClient, times(1)).fetchFileList(
                 TEST_ID_MINIAPP,
                 TEST_ID_MINIAPP_VERSION
             )
         }
+
+    @Test(expected = MiniAppSdkException::class)
+    fun `when downloading a mini app, MiniAppSdkException is thrown in case of invalid manifest`() =
+        runBlockingTest {
+            val downloader = getMockedDownloader()
+            When calling downloader.fetchManifest(
+                TEST_ID_MINIAPP,
+                TEST_ID_MINIAPP_VERSION
+            ) itReturns ManifestEntity(emptyList())
+            downloader.startDownload(
+                TEST_ID_MINIAPP,
+                TEST_ID_MINIAPP_VERSION
+            )
+        }
+
+    @Test
+    fun `isManifestValid returns true for valid Manifest`() {
+        val manifestEntity = ManifestEntity(files = listOf(TEST_URL_HTTPS_1, TEST_URL_HTTPS_2))
+        assertTrue { getMockedDownloader().isManifestValid(manifestEntity) }
+    }
+
+    @Test
+    fun `isManifestValid returns false when Manifest has empty list`() {
+        val manifestEntity = ManifestEntity(emptyList())
+        assertFalse { getMockedDownloader().isManifestValid(manifestEntity) }
+    }
+
+    @Test
+    fun `isManifestValid returns false when manifest is null`() {
+        val manifestEntity = Gson().fromJson("{}", ManifestEntity::class.java)
+        assertFalse { getMockedDownloader().isManifestValid(manifestEntity) }
+    }
+
+    @Test
+    fun `isManifestValid returns false when files list in manifest is null`() {
+        val manifestEntity = Gson().fromJson("""{"files": null}""", ManifestEntity::class.java)
+        assertFalse { getMockedDownloader().isManifestValid(manifestEntity) }
+    }
+
+    private fun getMockedDownloader(): MiniAppDownloader {
+        val apiClient: ApiClient = mock()
+        val storage: MiniAppStorage = mock()
+        val downloader = MiniAppDownloader(storage, apiClient)
+        return downloader
+    }
 }
