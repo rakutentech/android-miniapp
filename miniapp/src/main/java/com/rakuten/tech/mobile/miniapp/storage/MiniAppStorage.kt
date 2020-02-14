d9ba0ab6aedaadca8c564ac5b394bda0b34a9877
Nitish Jain <ts-nitish.a.jain@rakuten.com>
2020-02-14 14:20:36 +0900

    perf: make url parsing concurrent and update the unit tests

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/MiniAppStorage.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/MiniAppStorage.kt
@@ -2,6 +2,8 @@ package com.rakuten.tech.mobile.miniapp.storage
 
 import com.rakuten.tech.mobile.miniapp.MiniAppSdkException
 import com.rakuten.tech.mobile.miniapp.legacy.core.utils.LocalUrlParser
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.io.File
 import java.io.InputStream
 
@@ -18,16 +20,19 @@ internal class MiniAppStorage(
         source: String,
         basePath: String,
         inputStream: InputStream
-    ) {
-        try {
-            val filePath = getFilePath(source)
-            val fileName = getFileName(source)
-            fileWriter.write(inputStream, getAbsoluteWritePath(basePath, filePath, fileName))
-        } catch (error: Exception) {
-            // This should not happen unless BE sends in a differently "constructed" URL
-            // which differs in logic as that of LocalUrlParser
-            throw MiniAppSdkException(error)
+    ) = try {
+        coroutineScope {
+            val filePath = async { getFilePath(source) }
+            val fileName = async { getFileName(source) }
+            fileWriter.write(
+                inputStream,
+                getAbsoluteWritePath(basePath, filePath.await(), fileName.await())
+            )
         }
+    } catch (error: Exception) {
+        // This should not happen unless BE sends in a differently "constructed" URL
+        // which differs in logic as that of LocalUrlParser
+        throw MiniAppSdkException(error)
     }
 
     fun getAbsoluteWritePath(
@@ -36,9 +41,9 @@ internal class MiniAppStorage(
         fileName: String
     ) = "$basePath$filePath$fileName"
 
-    fun getFilePath(file: String) = localUrlParser.getFilePath(file)
+    suspend fun getFilePath(file: String) = localUrlParser.getFilePath(file)
 
-    fun getFileName(file: String) = localUrlParser.getFileName(file)
+    suspend fun getFileName(file: String) = localUrlParser.getFileName(file)
 
     fun getSavePathForApp(appId: String, versionId: String) =
         "${basePath.path}/$SUB_DIR_MINIAPP/$appId/$versionId"
