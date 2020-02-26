61e4b3608381c9aa9406e3ec4c16ff16e335771f
Nitish Jain <ts-nitish.a.jain@rakuten.com>
2020-02-26 11:09:26 +0900

    refactor: kotlinize url-to-file-info parser

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/UrlToFileInfoParser.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/UrlToFileInfoParser.kt
@@ -4,50 +4,37 @@ import java.io.File
 
 private const val KEY_MAP_PUB = "map-published"
 
-/**
- * URL parser. Manifest URL contains information such as MiniApp ID, Version ID.
- */
-class UrlToFileInfoParser {
+internal class UrlToFileInfoParser {
 
     /**
-     * Returns the path between versionId to file name.
-     * e.g. in
+     * Returns the path between versionId to file name e.g. for a given source,
      * "https://host.os.net/map-published/min-872f9172-804f-44e2-addd-ed612170dac9/
      * >>ver-6181004c-a6aa-4eda-b145-a5ff73fc4ad0/foo/bar/asset-manifest.json",
      * "/foo/bar/" will be returned.
      * Returns empty String when not found.
      */
     @Suppress("MagicNumber")
-    fun getFilePath(fileUrl: String): String {
-        val splitStrings: List<String> = fileUrl.split(File.separator)
-        val versionStringIndex = splitStrings.indexOf(KEY_MAP_PUB)
-        // If versionStringIndex = -1, version string was not found, then this URL is invalid.
-        if (versionStringIndex < 0) {
-            return ""
+    fun getFilePath(fileUrl: String): String = fileUrl.split(File.separator).run {
+        val versionIndex = indexOf(KEY_MAP_PUB)
+        return when {
+            // If versionIndex = -1, map-published was not found, then this source is invalid.
+            versionIndex < 0 -> ""
+            else -> {
+                var path = "/"
+                // The third string after "version" is the beginning of path.
+                subList(versionIndex + 3, lastIndex).forEach { path += "$it/" }
+                path
+            }
         }
-
-        // The second string after "version" is the beginning of path.
-        val startIndex = versionStringIndex + 3
-        val lastIndex = splitStrings.lastIndex
-
-        var path = "/"
-        for (str in splitStrings.subList(startIndex, lastIndex)) {
-            path += "$str/"
-        }
-
-        return path
     }
 
     /**
      * Returns the last element of the split string. Returns empty String when not found.
      */
-    fun getFileName(fileUrl: String): String {
-        val splitStrings: List<String> = fileUrl.split(File.separator)
-        val fileName = splitStrings.last()
-        if (fileName.contains(".")) {
-            return fileName
-        } else {
-            return ""
+    fun getFileName(fileUrl: String): String = fileUrl.split(File.separator).run {
+        return when {
+            "." in last() -> last()
+            else -> ""
         }
     }
 }
