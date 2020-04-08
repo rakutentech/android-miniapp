f3a6fef2891c9b74b5006097ed891afe522c506e
Nitish Jain <ts-nitish.a.jain@rakuten.com>
2020-04-08 16:53:44 +0900

    refactor: validate the mini app id in input activity

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/input/MiniAppInputActivity.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/input/MiniAppInputActivity.kt
@@ -8,6 +8,8 @@ import com.rakuten.tech.mobile.testapp.ui.display.MiniAppDisplayActivity
 import com.rakuten.tech.mobile.testapp.ui.miniapplist.MiniAppListActivity
 import com.rakuten.tech.mobile.testapp.ui.settings.SettingsMenuBaseActivity
 import kotlinx.android.synthetic.main.mini_app_input_activity.*
+import java.lang.Exception
+import java.util.UUID
 
 class MiniAppInputActivity : SettingsMenuBaseActivity() {
 
@@ -33,12 +35,19 @@ class MiniAppInputActivity : SettingsMenuBaseActivity() {
     }
 
     private fun isValidInput(edt: TextInputEditText): Boolean =
-        if (edt.text.toString().trim().isEmpty()) {
-            edt.error = getString(R.string.error_empty)
+        if (isInvalidUuid(edt.text.toString())) {
+            edt.error = getString(R.string.error_invalid_input)
             false
         } else {
             edt.error = null
             true
         }
+
+    private fun isInvalidUuid(input: String): Boolean = try {
+        UUID.fromString(input)
+        false
+    } catch (e: Exception) {
+        true
+    }
 }
 
