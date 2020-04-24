e21ac7daa66d82d27ace454cc15a028e8ee02ecc
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-04-24 11:21:56 +0900

    update: add new bridge js, uuid for unique id in sample.

--- miniapp/src/main/assets/bridge.js
+++ miniapp/src/main/assets/bridge.js
@@ -1,13 +1,14 @@
-var messageQueue = []
-window.MiniAppBridge = {};
+var MiniAppBridge = {};
+MiniAppBridge.messageQueue = [];
 var uniqueId = Math.random();
+
 var isPlatform = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    }
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
 };
 
 /**
@@ -18,17 +19,21 @@ var isPlatform = {
  * @param  {[Function]} onError Error callback function
  */
 MiniAppBridge.exec = function(action, onSuccess, onError) {
-    const callback = {}
-    callback.onSuccess = onSuccess;
-    callback.onError = onError;
-    callback.id = String(++uniqueId);
-    messageQueue.unshift(callback)
-    if(isPlatform.iOS()){
-        webkit.messageHandlers.MiniAppiOS.postMessage(JSON.stringify({action: action, id: callback.id}));
-    } else {
-        window.MiniAppAndroid.postMessage(JSON.stringify({action: action, id: callback.id}))
-    }
-}
+  const callback = {};
+  callback.onSuccess = onSuccess;
+  callback.onError = onError;
+  callback.id = String(++uniqueId);
+  MiniAppBridge.messageQueue.unshift(callback);
+  if (isPlatform.iOS()) {
+    webkit.messageHandlers.MiniAppiOS.postMessage(
+      JSON.stringify({ action: action, id: callback.id })
+    );
+  } else {
+    window.MiniAppAndroid.postMessage(
+      JSON.stringify({ action: action, id: callback.id })
+    );
+  }
+};
 
 /**
  * Success Callback method that will be called from native side
@@ -38,10 +43,16 @@ MiniAppBridge.exec = function(action, onSuccess, onError) {
  * @param  {[String]} value Response value sent from the native on invoking the action command
  */
 MiniAppBridge.execSuccessCallback = function(messageId, value) {
-    var queueObj = messageQueue.find(callback => callback.id = messageId)
+  var queueObj = MiniAppBridge.messageQueue.find(
+    (callback) => (callback.id = messageId)
+  );
+  if (value) {
     queueObj.onSuccess(value);
-    removeFromMessageQueue(queueObj)
-}
+  } else {
+    queueObj.onError("Unknown Error");
+  }
+  removeFromMessageQueue(queueObj);
+};
 
 /**
  * Error Callback method that will be called from native side
@@ -51,10 +62,15 @@ MiniAppBridge.execSuccessCallback = function(messageId, value) {
  * @param  {[String]} errorMessage Error message sent from the native on invoking the action command
  */
 MiniAppBridge.execErrorCallback = function(messageId, errorMessage) {
-    var queueObj = messageQueue.find(callback => callback.id = messageId)
-    queueObj.onError(errorMessage);
-    removeFromMessageQueue(queueObj)
-}
+  var queueObj = MiniAppBridge.messageQueue.find(
+    (callback) => (callback.id = messageId)
+  );
+  if (!errorMessage) {
+    errorMessage = "Unknown Error";
+  }
+  queueObj.onError(errorMessage);
+  removeFromMessageQueue(queueObj);
+};
 
 /**
  * Method to remove the callback object from the message queue after successfull/error communication
@@ -62,23 +78,23 @@ MiniAppBridge.execErrorCallback = function(messageId, errorMessage) {
  * @param  {[Object]} queueObj Queue Object that holds the references of callback informations
  */
 function removeFromMessageQueue(queueObj) {
-    var messageObjIndex = messageQueue.indexOf(queueObj)
-    if(messageObjIndex != -1) {
-        messageQueue.splice(messageObjIndex, 1);
-    }
+  var messageObjIndex = MiniAppBridge.messageQueue.indexOf(queueObj);
+  if (messageObjIndex != -1) {
+    MiniAppBridge.messageQueue.splice(messageObjIndex, 1);
+  }
 }
 
 /**
  * Associating getUniqueId function to MiniAppBridge object
-* @param  {[String]} messageId Message ID which will be used to get callback object from messageQueue
- * @param  {[String]} value Response value sent from the native on invoking the action command
  */
-MiniAppBridge.getUniqueId = function(messageId, value) {
-    return new Promise((resolve, reject) => {
-        return MiniAppBridge.exec(
-            "getUniqueId",
-            id => resolve(id),
-            error => reject(error)
-        );
-    })
-}
+MiniAppBridge.getUniqueId = function() {
+  return new Promise((resolve, reject) => {
+    return MiniAppBridge.exec(
+      "getUniqueId",
+      (id) => resolve(id),
+      (error) => reject(error)
+    );
+  });
+};
+window.MiniAppBridge = MiniAppBridge;
+module.exports = MiniAppBridge;
