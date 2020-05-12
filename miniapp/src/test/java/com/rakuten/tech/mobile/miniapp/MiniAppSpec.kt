429ad57f661bf8d462b30abac08e1c44c8149415
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-05-12 11:37:58 +0900

    test: update MiniApp

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppSpec.kt
@@ -1,10 +1,16 @@
 package com.rakuten.tech.mobile.miniapp
 
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import com.rakuten.tech.mobile.sdkutils.AppInfo
+import org.amshove.kluent.shouldNotBe
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MiniAppSpec {
 
     @Test
@@ -17,5 +23,18 @@ class MiniAppSpec {
         miniAppCompanion.instance(miniAppSdkConfig)
 
         verify(miniApp, times(1)).updateConfiguration(miniAppSdkConfig)
+    }
+
+    @Test
+    fun `MiniApp should init without problem`() {
+        AppInfo.instance = mock()
+        MiniApp.init(
+            context = getApplicationContext(),
+            baseUrl = TEST_URL_HTTPS_2,
+            rasAppId = TEST_HA_ID_APP,
+            subscriptionKey = TEST_HA_SUBSCRIPTION_KEY,
+            hostAppVersionId = TEST_HA_ID_VERSION
+        )
+        MiniApp.instance shouldNotBe null
     }
 }
