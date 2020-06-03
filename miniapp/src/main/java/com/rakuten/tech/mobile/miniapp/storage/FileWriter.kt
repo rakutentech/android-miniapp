3aacf963969c7858e3f872b13fd2d266a3cb6710
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-06-03 11:16:20 +0900

    test: metric

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/FileWriter.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/FileWriter.kt
@@ -2,8 +2,12 @@ package com.rakuten.tech.mobile.miniapp.storage
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 internal class FileWriter {
 
@@ -16,7 +20,34 @@ internal fun InputStream.toFile(path: String) {
     use { input ->
         File(path).apply {
             parentFile?.mkdirs()
-            outputStream().use { input.copyTo(it) }
+//            outputStream().use { input.copyTo(it) }
         }
     }
+}
+
+internal suspend fun ZipInputStream.decompress(basePath: String, fileZipPath: String) =
+    withContext(Dispatchers.IO) {
+        use { input ->
+            File(fileZipPath).apply { parentFile?.mkdirs() }
+            val outputFilePath = File(basePath)
+            var entry: ZipEntry
+
+            while (input.nextEntry.also { entry = it } != null) {
+                val newFile = File(outputFilePath, entry.name)
+                if (entry.isDirectory)
+                    newFile.mkdir()
+                else {
+                    var size: Int
+                    val buffer = ByteArray(2048)
+                    FileOutputStream(newFile).use { fos ->
+                        BufferedOutputStream(fos, buffer.size).use { bos ->
+                            while (input.read(buffer, 0, buffer.size).also { size = it } != -1) {
+                                bos.write(buffer, 0, size)
+                            }
+                            bos.flush()
+                        }
+                    }
+                }
+            }
+        }
 }
--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/MiniAppStorage.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/MiniAppStorage.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import java.io.File
 import java.io.InputStream
+import java.util.zip.ZipInputStream
 
 private const val SUB_DIR_MINIAPP = "miniapp"
 
@@ -28,9 +29,8 @@ internal class MiniAppStorage(
         try {
             val filePath = getFilePath(source)
             val fileName = getFileName(source)
-            fileWriter.write(inputStream, getAbsoluteWritePath(basePath, filePath, fileName))
-
-//            fileWriter.write(ZipInputStream(inputStream), getAbsoluteWritePath(basePath, filePath, fileName))
+//            fileWriter.write(inputStream, getAbsoluteWritePath(basePath, filePath, fileName))
+            ZipInputStream(inputStream).decompress(basePath, getAbsoluteWritePath(basePath, filePath, fileName))
         } catch (error: Exception) {
             // This should not happen unless BE sends in a differently "constructed" URL
             // which differs in logic as that of LocalUrlParser
