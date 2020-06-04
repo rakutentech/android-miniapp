b28c8af83c9c4def29dca7a705f44cd3b58bc22b
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-06-05 08:46:44 +0900

    refactor: cleanup

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloader.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloader.kt
@@ -1,6 +1,5 @@
 package com.rakuten.tech.mobile.miniapp
 
-import android.util.Log
 import androidx.annotation.VisibleForTesting
 import com.rakuten.tech.mobile.miniapp.api.ApiClient
 import com.rakuten.tech.mobile.miniapp.api.ManifestEntity
@@ -12,7 +11,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.Job
-import kotlin.system.measureTimeMillis
 
 internal class MiniAppDownloader(
     private val storage: MiniAppStorage,
@@ -45,9 +43,7 @@ internal class MiniAppDownloader(
     @VisibleForTesting
     suspend fun startDownload(appId: String, versionId: String): String {
         val manifest = fetchManifest(appId, versionId)
-        var path = ""
-        Log.d("MiniApp", "time execution:${measureTimeMillis { path = downloadMiniApp(appId, versionId, manifest) }}")
-        return path
+        return downloadMiniApp(appId, versionId, manifest)
     }
 
     @VisibleForTesting
