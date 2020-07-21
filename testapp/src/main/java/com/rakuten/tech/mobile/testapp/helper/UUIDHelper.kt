8633ed7d8dfcee5d14db54ab8e144628b439d59d
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-07-21 17:52:44 +0900

    add function to include hyphen for subscription key

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/UUIDHelper.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/UUIDHelper.kt
@@ -1,10 +1,14 @@
 package com.rakuten.tech.mobile.testapp.helper
 
+import java.lang.IllegalArgumentException
 import java.util.UUID
 
 fun String.isInvalidUuid(): Boolean = try {
     UUID.fromString(this)
     false
-} catch (e: Exception) {
+} catch (e: IllegalArgumentException) {
     true
 }
+
+fun String.includeHyphen(): String =
+    replace("(.{8})(.{4})(.{4})(.{4})(.+)".toRegex(), "$1-$2-$3-$4-$5")
