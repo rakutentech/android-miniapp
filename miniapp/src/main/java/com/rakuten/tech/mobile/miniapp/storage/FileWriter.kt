6b000b08d3715b1064f88e915b6922027ea00f15
Minh Nguyen (ミン） <60589925+minh-rakuten@users.noreply.github.com>
2020-08-05 10:57:14 +0900

    Feature/archive (#93)
    * test: metric
    
    * test: metric
    
    * test: metric
    
    * test: metric
    
    * feature: save and extract zip for miniapp
    
    * feature: load zip file
    
    * test: add test for unzip execution

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/FileWriter.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/storage/FileWriter.kt
@@ -1,22 +1,45 @@
 package com.rakuten.tech.mobile.miniapp.storage
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
-internal class FileWriter {
+internal class FileWriter(private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO) {
 
-    suspend fun write(inputStream: InputStream, path: String) = withContext(Dispatchers.IO) {
-        inputStream.toFile(path)
+    suspend fun unzip(inputStream: InputStream, zipPath: String) = withContext(coroutineDispatcher) {
+        ZipInputStream(inputStream).decompress(zipPath)
     }
 }
 
-internal fun InputStream.toFile(path: String) {
-    use { input ->
-        File(path).apply {
-            parentFile?.mkdirs()
-            outputStream().use { input.copyTo(it) }
+internal fun ZipInputStream.decompress(zipPath: String) = use { input ->
+    val outputFilePath = File(zipPath).apply { parentFile?.mkdirs() }
+    var entry: ZipEntry?
+
+    while (input.nextEntry.also { entry = it } != null) {
+        val newFile = File(outputFilePath.parentFile, entry!!.name)
+        if (entry!!.isDirectory)
+            newFile.mkdir()
+        else
+            writeFile(newFile, input)
+    }
+}
+
+@Suppress("MagicNumber")
+private fun writeFile(newFile: File, input: ZipInputStream) {
+    var size: Int
+    val buffer = ByteArray(2048)
+    FileOutputStream(newFile).use { fos ->
+        BufferedOutputStream(fos, buffer.size).use { bos ->
+            while (input.read(buffer, 0, buffer.size).also { size = it } != -1) {
+                bos.write(buffer, 0, size)
+            }
+            bos.flush()
         }
     }
 }
