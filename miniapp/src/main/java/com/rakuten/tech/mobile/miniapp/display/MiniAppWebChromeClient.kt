8c410283bc4d31efd680a8a0ddb05b0c07a54aec
Minh Nguyen (ミン） <60589925+minh-rakuten@users.noreply.github.com>
2020-08-19 15:28:09 +0900

    build: bridge submodule (#135)
    * build: submodule bridge
    
    * refactor: change bridge path
    
    * doc: submodule sparse-checkout
    
    * doc: note for sparse-checkout
    
    * doc: note for sparse-checkout
    
    * doc: note for sparse-checkout
    
    * ci: java 8 image
    
    * ci: update submodule
    
    * ci: remove extra files
    
    * doc: note move out bash block
    
    * ci: add ToDo sparse-checkout
    
    * ci: add ToDo sparse-checkout
    
    * ci: add ToDo sparse-checkout
    
    * ci: add ToDo sparse-checkout

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/display/MiniAppWebChromeClient.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/display/MiniAppWebChromeClient.kt
@@ -19,7 +19,7 @@ internal class MiniAppWebChromeClient(
     @Suppress("TooGenericExceptionCaught", "SwallowedException")
     @VisibleForTesting
     internal val bridgeJs = try {
-        val inputStream = context.assets.open("bridge.js")
+        val inputStream = context.assets.open("js-miniapp/js-miniapp-bridge/export/android/bridge.js")
         inputStream.bufferedReader().use(BufferedReader::readText)
     } catch (e: Exception) {
         null
