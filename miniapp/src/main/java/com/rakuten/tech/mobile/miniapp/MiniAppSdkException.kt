10b4c433c11fe881d9c0692451717ad1ce84cd25
Cory Caywood <cory@floatinginspace.net>
2020-08-31 11:58:59 +0900

    fix: Return custom message for NetworkOnMainThreadException
    This exception is thrown when the SDK user tries to call a suspending function from Dispatchers.Main. Also updated the MiniAppSdkException to accept the 'cause'

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppSdkException.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppSdkException.kt
@@ -1,17 +1,23 @@
 package com.rakuten.tech.mobile.miniapp
 
+import android.os.NetworkOnMainThreadException
+
 /**
  * A custom exception class which treats the purpose of providing
  * error information to the consumer app in an unified way.
  */
-open class MiniAppSdkException(message: String) : Exception(message) {
+open class MiniAppSdkException(message: String, cause: Throwable?) : Exception(message, cause) {
 
-    constructor(e: Exception) : this("Found some problem, ${e.message}")
+    constructor(e: Exception) : this(exceptionMessage(e), e)
+
+    constructor(message: String) : this(message, null)
 }
 
-internal class MiniAppNetException(message: String) : MiniAppSdkException(message) {
+internal class MiniAppNetException(message: String, cause: Throwable?) : MiniAppSdkException(message, cause) {
 
-    constructor(e: Exception) : this("Found some problem, ${e.message}")
+    constructor(e: Exception) : this("Found some problem, ${e.message}", e)
+
+    constructor(message: String) : this(message, null)
 }
 
 @Suppress("FunctionMaxLength")
@@ -29,3 +35,9 @@ internal fun sdkExceptionForInvalidArguments(message: String = "") =
 @Suppress("FunctionMaxLength")
 internal fun sdkExceptionForNoActivityContext() =
     MiniAppSdkException("Only accept context of type Activity or ActivityCompat")
+
+private fun exceptionMessage(exception: Exception) = when (exception) {
+    is NetworkOnMainThreadException -> "Network requests must not be performed on the main thread. "
+        .plus("Use Dispatchers.IO or Dispatchers.Default for MiniApp suspending functions.")
+    else -> "Found some problem, ${exception.message}"
+}
