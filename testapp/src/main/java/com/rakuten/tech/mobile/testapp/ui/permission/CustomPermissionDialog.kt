d7f8acba904aec3e9fc75de691cf9546043a0a76
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-08-31 12:14:07 +0900

    UI beautify

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/permission/CustomPermissionDialog.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/permission/CustomPermissionDialog.kt
@@ -3,7 +3,12 @@ package com.rakuten.tech.mobile.testapp.ui.permission
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 
 class CustomPermissionDialog {
 
@@ -12,7 +17,7 @@ class CustomPermissionDialog {
 
         fun build(context: Context?, miniAppName: String): Builder {
             alert = AlertDialog.Builder(context)
-            alert?.setMessage("Are you sure you don't want to allow any of the permissions for $miniAppName?")
+            setTitle(context, miniAppName)
             return this
         }
 
@@ -31,6 +36,22 @@ class CustomPermissionDialog {
                 if (!it.isShowing)
                     it.show()
             }
+        }
+
+        private fun setTitle(context: Context?, miniAppName: String) {
+            val title = TextView(context)
+            title.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            title.setPadding(4, 18, 4, 18)
+            title.setBackgroundColor(Color.parseColor("#f2f3f4"))
+            title.setTextColor(Color.BLACK)
+            title.text =
+                "Are you sure you don't want to allow any of the permissions for $miniAppName?"
+            title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+            title.gravity = Gravity.CENTER
+            alert?.setCustomTitle(title)
         }
     }
 }
