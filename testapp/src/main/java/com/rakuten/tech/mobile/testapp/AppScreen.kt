a7df21cbdff62560410470364bb5e857a25318d7
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-09-10 13:51:25 +0900

    support navigation

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/AppScreen.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/AppScreen.kt
@@ -2,8 +2,10 @@ package com.rakuten.tech.mobile.testapp
 
 import com.rakuten.tech.mobile.testapp.ui.input.MiniAppInputActivity
 import com.rakuten.tech.mobile.testapp.ui.miniapplist.MiniAppListActivity
+import com.rakuten.tech.mobile.testapp.ui.settings.SettingsMenuActivity
 
 object AppScreen {
     val MINI_APP_LIST_ACTIVITY = MiniAppListActivity::class.java.canonicalName
     val MINI_APP_INPUT_ACTIVITY = MiniAppInputActivity::class.java.canonicalName
+    val MINI_APP_SETTINGS_ACTIVITY = SettingsMenuActivity::class.java.canonicalName
 }
--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ProfileSettingsActivity.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ProfileSettingsActivity.kt
@@ -4,8 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -19,6 +21,8 @@ import kotlin.properties.Delegates
 class ProfileSettingsActivity : BaseActivity() {
 
     companion object {
+        private const val PICK_IMAGE = 1001
+
         fun start(activity: Activity) {
             val intent = Intent(activity, ProfileSettingsActivity::class.java)
             intent.putExtra(
@@ -41,6 +45,7 @@ class ProfileSettingsActivity : BaseActivity() {
         setContentView(R.layout.profile_settings_activity)
         initializeActionBar()
         setIcon(null)
+        setImageListener()
     }
 
     private fun initializeActionBar() {
@@ -75,14 +80,33 @@ class ProfileSettingsActivity : BaseActivity() {
         )
     }
 
+    private fun setImageListener() {
+        textAddPhoto.setOnClickListener {
+            openGallery()
+        }
+    }
+
     private fun updateSettings(name: String) {
+    }
+
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, PICK_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
+            setIcon(data?.data)
+        }
     }
 
     private fun setIcon(uri: Uri?) {
         Glide.with(this@ProfileSettingsActivity)
             .load(uri).apply(RequestOptions().circleCrop())
             .placeholder(R.drawable.r_logo)
-            .into(imageProfile)
+            .into(imageProfile as ImageView)
     }
 
     private fun navigateToPreviousScreen() {
