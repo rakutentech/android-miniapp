80bd716e2de6b07c1c66c33661a58a2497203adf
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-09-16 21:30:03 +0900

    refactor: whenAdMobProvided

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/ads/AdMob.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/ads/AdMob.kt
@@ -4,11 +4,10 @@ package com.rakuten.tech.mobile.miniapp.ads
  * Check whether hostapp provides AdMob dependency.
  * If the dependency is provided then it means the AdMob app id has also been set from hostapp.
  */
+@Suppress("EmptyCatchBlock", "SwallowedException")
 inline fun <T> whenAdMobProvided(callback: () -> T) {
     try {
         Class.forName("com.google.android.gms.ads.MobileAds")
         callback.invoke()
-    } catch (e: ClassNotFoundException) {
-        e.printStackTrace()
-    }
+    } catch (e: ClassNotFoundException) {}
 }
