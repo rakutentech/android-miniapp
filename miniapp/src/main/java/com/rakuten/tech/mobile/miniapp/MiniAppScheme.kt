dcf20a7edc2c97f754b6a4f02c0d7a2f4f8c21cd
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-09-23 17:17:28 +0900

    refactor: add custom scheme DSL

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppScheme.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/MiniAppScheme.kt
@@ -7,4 +7,10 @@ internal class MiniAppScheme(miniAppId: String) {
     val miniAppCustomDomain = "https://$miniAppDomain/"
 
     fun isMiniAppUrl(url: String) = url.startsWith(miniAppCustomDomain) || url.startsWith(miniAppCustomScheme)
+
+    internal inline fun <T, R> ifLoadingCustomScheme(url: T, callback: (String) -> R): R? =
+        if (url is String && url.startsWith(miniAppCustomScheme))
+            url.replace(miniAppCustomScheme, miniAppCustomDomain).let(callback)
+        else
+            null
 }
