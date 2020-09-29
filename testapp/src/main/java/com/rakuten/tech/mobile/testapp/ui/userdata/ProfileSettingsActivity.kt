73e62f9e18862c50fbe05dcd3776d7eec0aea04f
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-09-29 10:53:26 +0900

    enable save button in profile screen

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ProfileSettingsActivity.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ProfileSettingsActivity.kt
@@ -17,18 +17,11 @@ import com.rakuten.tech.mobile.testapp.helper.isInputEmpty
 import com.rakuten.tech.mobile.testapp.ui.base.BaseActivity
 import com.rakuten.tech.mobile.testapp.ui.settings.AppSettings
 import kotlinx.android.synthetic.main.profile_settings_activity.*
-import kotlin.properties.Delegates
 
 class ProfileSettingsActivity : BaseActivity() {
 
     private lateinit var settings: AppSettings
     private lateinit var profileUrl: String
-
-    private var saveViewEnabled by Delegates.observable(true) { _, old, new ->
-        if (new != old) {
-            invalidateOptionsMenu()
-        }
-    }
 
     private val nameTextWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {}
@@ -52,7 +45,6 @@ class ProfileSettingsActivity : BaseActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.settings_menu, menu)
-        menu.findItem(R.id.settings_menu_save).isEnabled = saveViewEnabled
         return true
     }
 
@@ -94,7 +86,6 @@ class ProfileSettingsActivity : BaseActivity() {
     }
 
     private fun validateNameInput() {
-        saveViewEnabled = !isInputEmpty(editProfileName)
         if (isInputEmpty(editProfileName)) {
             editProfileName.error = getString(R.string.userdata_error_invalid_name)
         }
