15df9f7f046621ecc91ccb40f8a2c772aa702946
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-10-21 16:34:05 +0900

    modify logic to support more usecases

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/js/KeyboardBridgeDispatcher.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/js/KeyboardBridgeDispatcher.kt
@@ -2,6 +2,9 @@ package com.rakuten.tech.mobile.miniapp.js
 
 import android.app.Activity
 import android.content.Context
+import android.os.Bundle
+import android.os.ResultReceiver
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 
 internal class KeyboardBridgeDispatcher(
@@ -11,15 +14,43 @@ internal class KeyboardBridgeDispatcher(
 
     fun onGetKeyboardVisibility(callbackId: String) {
         try {
+            val immResultReceiver = IMMResultReceiver()
+            val view = activity.findViewById(android.R.id.content) as ViewGroup
             val inputMethodManager: InputMethodManager =
                 activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-            if (inputMethodManager.isActive && inputMethodManager.isAcceptingText)
+            inputMethodManager.showSoftInput(view.getChildAt(0), 0, immResultReceiver)
+            val result = immResultReceiver.findResult()
+            if (result == InputMethodManager.RESULT_UNCHANGED_SHOWN ||
+                result == InputMethodManager.RESULT_UNCHANGED_HIDDEN
+            ) {
                 bridgeExecutor.postValue(callbackId, VISIBLE)
-            else
+            } else {
                 bridgeExecutor.postValue(callbackId, INVISIBLE)
+            }
         } catch (e: Exception) {
             bridgeExecutor.postError(callbackId, "$ERR_KEYBOARD_VISIBILITY ${e.message}")
+        }
+    }
+
+    private class IMMResultReceiver : ResultReceiver(null) {
+        var currentResult = -1
+
+        override fun onReceiveResult(r: Int, data: Bundle?) {
+            currentResult = r
+        }
+
+        fun findResult(): Int {
+            try {
+                var sleep = 0
+                while (currentResult == -1 && sleep < 500) {
+                    Thread.sleep(100)
+                    sleep += 100
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+
+            return currentResult
         }
     }
 
