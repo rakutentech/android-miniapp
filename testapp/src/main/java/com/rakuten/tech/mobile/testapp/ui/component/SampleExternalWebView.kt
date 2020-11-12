d803c2f21eeedc14a312c87d314c1d872c689e38
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-11-12 17:36:40 +0900

    doc: add changelog

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/component/SampleExternalWebView.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/component/SampleExternalWebView.kt
@@ -14,6 +14,7 @@ class SampleExternalWebView(context: Context, url: String, sampleWebViewClient: 
         settings.allowUniversalAccessFromFileURLs = true
         settings.domStorageEnabled = true
         settings.databaseEnabled = true
+        settings.mediaPlaybackRequiresUserGesture = false
         webViewClient = sampleWebViewClient
         loadUrl(url)
     }
