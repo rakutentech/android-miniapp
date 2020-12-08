88a8aaf2d5111bc1c5aa4ee31b00607f2995bbaa
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2020-12-08 10:56:46 +0900

    add getContacts interface

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/js/userinfo/UserInfoBridgeDispatcher.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/js/userinfo/UserInfoBridgeDispatcher.kt
@@ -41,6 +41,17 @@ abstract class UserInfoBridgeDispatcher {
         throw MiniAppSdkException("The `UserInfoBridgeDispatcher.getAccessToken` $NO_IMPL")
     }
 
+    /**
+     * Get contacts from host app.
+     * You can also throw an [Exception] from this method to pass an error message to the mini app.
+     */
+    open fun getContacts(
+        onSuccess: (contactIdsAsJson: String) -> Unit,
+        onError: (message: String) -> Unit
+    ) {
+        throw MiniAppSdkException("The `UserInfoBridgeDispatcher.getContacts` $NO_IMPL")
+    }
+
     internal fun init(
         bridgeExecutor: MiniAppBridgeExecutor,
         miniAppCustomPermissionCache: MiniAppCustomPermissionCache,
@@ -115,6 +126,19 @@ abstract class UserInfoBridgeDispatcher {
         bridgeExecutor.postError(callbackId, "$ERR_GET_ACCESS_TOKEN ${e.message}")
     }
 
+    internal fun onGetContacts(callbackId: String) = try {
+        val successCallback = { contactIdsAsJson: String ->
+            bridgeExecutor.postValue(callbackId, contactIdsAsJson)
+        }
+        val errorCallback = { message: String ->
+            bridgeExecutor.postError(callbackId, "$ERR_GET_CONTACTS $message")
+        }
+
+        getContacts(successCallback, errorCallback)
+    } catch (e: Exception) {
+        bridgeExecutor.postError(callbackId, "$ERR_GET_CONTACTS ${e.message}")
+    }
+
     @VisibleForTesting
     internal companion object {
         const val NO_IMPL = "method has not been implemented by the Host App."
@@ -125,5 +149,6 @@ abstract class UserInfoBridgeDispatcher {
         const val ERR_PROFILE_PHOTO_NO_PERMISSION =
             "Permission has not been accepted yet for getting profile photo."
         const val ERR_GET_ACCESS_TOKEN = "Cannot get access token:"
+        const val ERR_GET_CONTACTS = "Cannot get contacts:"
     }
 }
