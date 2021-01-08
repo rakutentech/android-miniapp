7a7d6e15c513d90f1dfd36c0f6d87598db719524
ts-mdkhairul.alam <ts-mdkhairul.alam@rakuten.com>
2021-01-08 16:24:45 +0900

    cleanup

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/AppPermission.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/AppPermission.kt
@@ -2,22 +2,11 @@ package com.rakuten.tech.mobile.testapp.helper
 
 import android.Manifest
 import com.rakuten.tech.mobile.miniapp.permission.MiniAppDevicePermissionType
-import com.rakuten.tech.mobile.miniapp.permission.MiniAppPermissionType
 
 object AppPermission {
 
     object ReqCode {
         const val LOCATION = 1001
-    }
-
-    fun getRequestCode(permissionType: MiniAppPermissionType) = when (permissionType) {
-        MiniAppPermissionType.LOCATION -> ReqCode.LOCATION
-        else -> 0
-    }
-
-    fun getPermissionRequest(permissionType: MiniAppPermissionType) = when (permissionType) {
-        MiniAppPermissionType.LOCATION -> arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-        else -> emptyArray()
     }
 
     fun getDeviceRequestCode(permissionType: MiniAppDevicePermissionType) = when (permissionType) {
