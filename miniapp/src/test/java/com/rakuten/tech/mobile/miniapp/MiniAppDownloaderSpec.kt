f47154c37c79814f65f5617a6b5150509502b551
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2021-02-22 19:35:34 +0900

    build: fix okhttp gradle sync

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloaderSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/MiniAppDownloaderSpec.kt
@@ -297,7 +297,7 @@ class MiniAppDownloaderSpec {
             TEST_ID_MINIAPP_VERSION
         ) itReturns ManifestEntity(listOf(TEST_URL_HTTPS_1))
 
-        val mockResponseBody: ResponseBody = TEST_BODY_CONTENT.toResponseBody(null)
+        val mockResponseBody = TEST_BODY_CONTENT.toResponseBody(null)
         When calling apiClient.downloadFile(TEST_URL_HTTPS_1) itReturns mockResponseBody
     }
 
