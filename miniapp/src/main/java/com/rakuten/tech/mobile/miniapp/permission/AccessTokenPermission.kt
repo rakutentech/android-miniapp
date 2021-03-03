b1b07a35c44eafe13dbec716ac4275c229b86758
minh-rakuten <ts-minh.nguyen@rakuten.com>
2021-03-03 17:53:33 +0900

    feat: AccessTokenPermission implementation

--- miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/permission/AccessTokenPermission.kt
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/permission/AccessTokenPermission.kt
@@ -1,4 +1,15 @@
 package com.rakuten.tech.mobile.miniapp.permission
 
-class AccessTokenPermission {
+import androidx.annotation.Keep
+
+/**
+ *  Contains the components which need to be validated when access token is granted.
+ *  @property audience The service of access token.
+ *  @property scopes List of areas that token can access.
+ */
+@Keep
+data class AccessTokenPermission(val audience: String, val scopes: List<String>) {
+
+    fun contains(accessTokenPermission: AccessTokenPermission): Boolean =
+        this.audience == accessTokenPermission.audience && this.scopes.containsAll(accessTokenPermission.scopes)
 }
