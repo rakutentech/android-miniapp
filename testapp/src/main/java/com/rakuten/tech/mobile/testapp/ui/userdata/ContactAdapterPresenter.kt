516bd141f960ee95df4df23a33d6fcd1312d122e
MD MUNIR HOSSAIN <84305007+munir-rakuten@users.noreply.github.com>
2021-08-24 13:57:25 +0900

    MINI-2915 Editable contact list in app settings (#355)
    * contact list can be editable
    
    * update changelog
    
    * some minor refactoring

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ContactAdapterPresenter.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/ContactAdapterPresenter.kt
@@ -4,6 +4,7 @@ import com.rakuten.tech.mobile.miniapp.js.userinfo.Contact
 
 interface ContactAdapterPresenter {
     fun addContact(position: Int, contact: Contact)
+    fun updateContact(position: Int, contact: Contact)
     fun addContactList(contacts: ArrayList<Contact>)
     fun removeContactAt(position: Int)
     fun provideContactEntries(): ArrayList<Contact>
