0b0eb3a4ff3e32579b1daf4b13c6623d7314bc8e
Khairul Alam Licon <mdkhairul.alam@rakuten.com>
2021-08-27 16:35:53 +0900

    adopt signature verifier sdk

---
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/signatureverifier/SignatureVerifier.kt
@@ -0,0 +1,73 @@
+package com.rakuten.tech.mobile.miniapp.signatureverifier
+
+import android.content.Context
+import com.rakuten.tech.mobile.miniapp.signatureverifier.api.ApiClient
+import com.rakuten.tech.mobile.miniapp.signatureverifier.api.PublicKeyFetcher
+import com.rakuten.tech.mobile.miniapp.signatureverifier.verification.PublicKeyCache
+import java.io.InputStream
+
+/**
+ * Main entry point for the Signature Verifier SDK.
+ * Should be accessed via [SignatureVerifier.instance].
+ */
+@Suppress("UnnecessaryAbstractClass")
+abstract class SignatureVerifier {
+
+    /**
+     * Verifies the [signature] of the [data] using the [publicKeyId].
+     *
+     * @return true if [signature] associated with [data] is valid.
+     */
+    abstract suspend fun verify(publicKeyId: String, data: InputStream, signature: String): Boolean
+
+    companion object {
+        internal var callback: ((ex: Exception) -> Unit)? = null
+
+        /**
+         * Initializes an instance of the Signature Verifier SDK based on the provided parameters.
+         *
+         * @param [context] application context
+         * @param [baseUrl] endpoint used for public key fetching
+         * @param [subscriptionKey] authorization key for the public key fetching endpoint
+         * @param [errorCallback] optional callback function for app to receive any exception occurred in the SDK.
+         *
+         * @return `instance` if initialization is successful, and `null` otherwise.
+         */
+        @SuppressWarnings("LongMethod", "TooGenericExceptionCaught")
+        fun init(
+                context: Context,
+                baseUrl: String,
+                subscriptionKey: String,
+                errorCallback: ((ex: Exception) -> Unit)? = null
+        ): SignatureVerifier? {
+            callback = errorCallback
+            return try {
+
+                val client = ApiClient(
+                        baseUrl = baseUrl,
+                        subscriptionKey = subscriptionKey,
+                        context = context
+                )
+
+                RealSignatureVerifier(
+                        PublicKeyCache(
+                                keyFetcher = PublicKeyFetcher(client),
+                                context = context,
+                                baseUrl = baseUrl
+                        )
+                )
+            } catch (ex: Exception) {
+                callback?.let {
+                    it(SignatureVerifierException("Signature Verifier initialization failed", ex))
+                }
+                null
+            }
+        }
+    }
+}
+
+/**
+ * Custom exception for Signature Verifier SDK.
+ */
+class SignatureVerifierException(name: String, cause: Throwable? = null) :
+        RuntimeException(name, cause)
---
+++ miniapp/src/main/java/com/rakuten/tech/mobile/miniapp/signatureverifier/verification/PublicKeyCache.kt
@@ -0,0 +1,86 @@
+package com.rakuten.tech.mobile.miniapp.signatureverifier.verification
+
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.rakuten.tech.mobile.miniapp.signatureverifier.SignatureVerifier
+import com.rakuten.tech.mobile.miniapp.signatureverifier.api.PublicKeyFetcher
+import java.io.File
+
+internal class PublicKeyCache(
+        private val keyFetcher: PublicKeyFetcher,
+        context: Context,
+        baseUrl: String,
+        encryptor: AesEncryptor? = null,
+        testKeys: MutableMap<String, String>? = null
+) {
+
+    private val encryptor: AesEncryptor by lazy { encryptor ?: AesEncryptor() }
+
+    private val file: File by lazy {
+        // replace all non-alphanumeric characters to '.':
+        // - multiple/group of non-alphanumeric will be replace by one '.'
+        // - trailing period is removed
+        val filepath = baseUrl.replace(Regex("[^a-zA-Z0-9]+"), ".").replace(Regex(".$"), "")
+        File(
+                context.noBackupFilesDir,
+                ("signature.keys.$filepath").take(MAX_PATH)
+        )
+    }
+
+    @VisibleForTesting
+    internal val keys: MutableMap<String, String> by lazy {
+        testKeys
+                ?: if (file.exists()) {
+                    val text = file.readText()
+
+                    if (text.isNotBlank()) {
+                        val type = object : TypeToken<MutableMap<String, String>>() {}.type
+                        Gson().fromJson(text, type)
+                    } else {
+                        mutableMapOf()
+                    }
+                } else {
+                    mutableMapOf()
+                }
+    }
+
+    operator fun get(keyId: String): String? {
+        val encryptedKey = keys[keyId]
+
+        return if (encryptedKey != null) {
+            encryptor.decrypt(encryptedKey) ?: fetch(keyId)
+        } else {
+            fetch(keyId)
+        }
+    }
+
+    fun remove(keyId: String) {
+        keys.remove(keyId)
+
+        file.writeText(Gson().toJson(keys))
+    }
+
+    @SuppressWarnings("TooGenericExceptionCaught")
+    private fun fetch(keyId: String): String? {
+        val key = try {
+            keyFetcher.fetch(keyId)
+        } catch (ex: Exception) {
+            SignatureVerifier.callback?.let { it(ex) }
+            null
+        }
+        if (!key.isNullOrEmpty()) {
+            encryptor.encrypt(key)?.let {
+                keys[keyId] = it
+                file.writeText(Gson().toJson(keys))
+            }
+        }
+
+        return key
+    }
+
+    companion object {
+        private const val MAX_PATH = 100
+    }
+}
