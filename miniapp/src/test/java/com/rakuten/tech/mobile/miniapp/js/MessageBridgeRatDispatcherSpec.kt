4958841787dc3cd0f1dd936b73b329455ecc22ab
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2021-08-31 13:33:19 +0900

    added more cases to updatecoverage

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MessageBridgeRatDispatcherSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MessageBridgeRatDispatcherSpec.kt
@@ -16,8 +16,27 @@ import kotlin.test.assertEquals
 class MessageBridgeRatDispatcherSpec {
 
     private var miniAppAnalytics: MiniAppAnalytics = mock()
-    private val ratDispatcher: MessageBridgeRatDispatcher = MessageBridgeRatDispatcher(miniAppAnalytics)
+    private val ratDispatcher: MessageBridgeRatDispatcher = MessageBridgeRatDispatcher(
+        miniAppAnalytics
+    )
     private val testAction = ActionType.GET_ACCESS_TOKEN.action
+
+    private val testActionValues = mapOf<String, Actype>(
+        ActionType.REQUEST_PERMISSION.action to Actype.REQUEST_PERMISSION,
+        ActionType.REQUEST_CUSTOM_PERMISSIONS.action to Actype.REQUEST_CUSTOM_PERMISSIONS,
+        ActionType.SHARE_INFO.action to Actype.SHARE_INFO,
+        ActionType.LOAD_AD.action to Actype.LOAD_AD,
+        ActionType.SHOW_AD.action to Actype.SHOW_AD,
+        ActionType.GET_USER_NAME.action to Actype.GET_USER_NAME,
+        ActionType.GET_PROFILE_PHOTO.action to Actype.GET_PROFILE_PHOTO,
+        ActionType.GET_ACCESS_TOKEN.action to Actype.GET_ACCESS_TOKEN,
+        ActionType.SET_SCREEN_ORIENTATION.action to Actype.SET_SCREEN_ORIENTATION,
+        ActionType.GET_CONTACTS.action to Actype.GET_CONTACTS,
+        ActionType.SEND_MESSAGE_TO_CONTACT.action to Actype.SEND_MESSAGE_TO_CONTACT,
+        ActionType.SEND_MESSAGE_TO_CONTACT_ID.action to Actype.SEND_MESSAGE_TO_CONTACT_ID,
+        ActionType.SEND_MESSAGE_TO_MULTIPLE_CONTACTS.action to Actype.SEND_MESSAGE_TO_MULTIPLE_CONTACTS,
+        ActionType.GET_POINTS.action to Actype.GET_POINTS
+    )
 
     @Test
     fun `should send analytics with correct params`() = runBlockingTest {
@@ -31,8 +50,10 @@ class MessageBridgeRatDispatcherSpec {
 
     @Test
     fun `getAcType should return the correct acType for correct action`() {
-        val acType = ratDispatcher.getAcType(testAction)
-        assertEquals(expected = Actype.GET_ACCESS_TOKEN, actual = acType)
+        for (entry in testActionValues.entries) {
+            val acType = ratDispatcher.getAcType(entry.key)
+            assertEquals(expected = entry.value, actual = acType)
+        }
     }
 
     @Test
