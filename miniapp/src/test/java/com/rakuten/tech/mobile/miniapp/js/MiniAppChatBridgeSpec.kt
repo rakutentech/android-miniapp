6efc510b11462786a017834e85df3cbf8a734635
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2021-10-08 12:26:09 +0900

    update test case

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MiniAppChatBridgeSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MiniAppChatBridgeSpec.kt
@@ -71,6 +71,10 @@ open class BridgeCommon {
             override fun runErrorCallback(callbackId: String, errorMessage: String) {
                 Assert.assertEquals(errorMessage, errMsg)
             }
+
+            override fun runNativeEventCallback(eventType: String, value: String) {
+                throw Exception()
+            }
         }
 }
 
