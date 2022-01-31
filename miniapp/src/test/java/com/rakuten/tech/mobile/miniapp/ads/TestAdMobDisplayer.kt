95943cbb6cde7ec803e9638345ec7fe1e14a36dd
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2022-01-31 17:56:38 +0900

    added test class

---
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/ads/TestAdMobDisplayer.kt
@@ -0,0 +1,47 @@
+package com.rakuten.tech.mobile.miniapp.ads
+
+import androidx.annotation.VisibleForTesting
+import com.rakuten.tech.mobile.miniapp.TEST_ERROR_MSG
+
+@Suppress("TooGenericExceptionThrown")
+internal class TestAdMobDisplayer : MiniAppAdDisplayer {
+    override fun loadInterstitialAd(
+        adUnitId: String,
+        onLoaded: () -> Unit,
+        onFailed: (String) -> Unit
+    ) {
+        onLoaded.invoke()
+        onFailed.invoke(TEST_ERROR_MSG)
+        throw Exception()
+    }
+
+    override fun showInterstitialAd(
+        adUnitId: String,
+        onClosed: () -> Unit,
+        onFailed: (String) -> Unit
+    ) {
+        onClosed.invoke()
+        onFailed.invoke(TEST_ERROR_MSG)
+        throw Exception()
+    }
+
+    override fun loadRewardedAd(adUnitId: String, onLoaded: () -> Unit, onFailed: (String) -> Unit) {
+        onLoaded.invoke()
+        onFailed.invoke(TEST_ERROR_MSG)
+        throw Exception()
+    }
+
+    override fun showRewardedAd(
+        adUnitId: String,
+        onClosed: (reward: Reward?) -> Unit,
+        onFailed: (String) -> Unit
+    ) {
+        onClosed.invoke(null)
+        onClosed.invoke(Reward("", 1))
+        onFailed.invoke(TEST_ERROR_MSG)
+        throw Exception()
+    }
+}
+
+@VisibleForTesting
+internal var AdMobClassName = "com.google.android.gms.ads.MobileAds"
