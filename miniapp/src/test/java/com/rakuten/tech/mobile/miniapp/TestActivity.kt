7a19314666a1c6a7c34c8c87ab21d74a311b2513
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2022-04-27 19:15:42 +0900

    remove file path override from test activity

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/TestActivity.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/TestActivity.kt
@@ -1,9 +1,5 @@
 package com.rakuten.tech.mobile.miniapp
 
 import android.app.Activity
-import java.io.File
 
-class TestActivity : Activity() {
-    private val filesDir = ""
-    override fun getFilesDir(): File = File(filesDir)
-}
+class TestActivity : Activity()
