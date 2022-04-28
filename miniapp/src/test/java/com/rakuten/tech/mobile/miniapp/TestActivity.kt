a8058594f8bc9085c59299fd67e322ff2961d6a7
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2022-04-28 12:21:12 +0900

    update unit test

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/TestActivity.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/TestActivity.kt
@@ -1,5 +1,11 @@
 package com.rakuten.tech.mobile.miniapp
 
 import android.app.Activity
+import java.io.File
 
 class TestActivity : Activity()
+class TestFilesDirActivity : Activity() {
+    override fun getFilesDir(): File {
+        return File("")
+    }
+}
