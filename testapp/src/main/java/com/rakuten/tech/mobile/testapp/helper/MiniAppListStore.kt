93d71a8cb2cf6a0fc421e79e2bd767f878df28bf
ts-mdmunir.hossain <ts-mdmunir.hossain@rakuten.com>
2022-05-02 16:26:44 +0900

    remove unused imports

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/MiniAppListStore.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/helper/MiniAppListStore.kt
@@ -7,7 +7,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.rakuten.tech.mobile.miniapp.MiniAppInfo
 import com.rakuten.tech.mobile.testapp.ui.settings.AppSettings
-import java.lang.IllegalStateException
 
 class MiniAppListStore private constructor(context: Context) {
 
