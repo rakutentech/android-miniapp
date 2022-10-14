48726c21d5bd1afdc7bc271112bda7f8664e1e7d
Shohieb <shohiebsense@users.noreply.github.com>
2022-10-14 23:43:07 +0900

    fix MiniAppH5AdsProvider

--- admob-latest/src/main/java/com/rakuten/tech/mobile/miniapp/ads/MiniAppH5AdsProvider.kt
+++ admob-latest/src/main/java/com/rakuten/tech/mobile/miniapp/ads/MiniAppH5AdsProvider.kt
@@ -5,23 +5,3 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.google.android.gms.ads.h5.H5AdsWebViewClient
 
-/**
- * The class for providing functions related to H5AdsWebViewClient for using in a WebView.
- */
-class MiniAppH5AdsProvider {
-    /**
-     * The function to create a H5AdsWebViewClient.
-     * @param context context of the webview.
-     * @param webView the webview which needs to enable H5Ads.
-     * @param webViewClient the WebViewClient for delegating to H5AdsWebViewClient.
-     */
-    fun getH5AdsWebViewClient(
-        context: Context,
-        webView: WebView,
-        webViewClient: WebViewClient
-    ): H5AdsWebViewClient {
-        val h5Client = H5AdsWebViewClient(context, webView)
-        h5Client.delegateWebViewClient = webViewClient
-        return h5Client
-    }
-}
