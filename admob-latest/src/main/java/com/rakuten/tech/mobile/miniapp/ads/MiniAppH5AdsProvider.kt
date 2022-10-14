35e62b20d71ec3062b31cd75508e4c16a9685a9b
Shohieb <shohiebsense@users.noreply.github.com>
2022-10-14 23:49:15 +0900

    fix MiniAppH5AdsProvider

--- admob-latest/src/main/java/com/rakuten/tech/mobile/miniapp/ads/MiniAppH5AdsProvider.kt
+++ admob-latest/src/main/java/com/rakuten/tech/mobile/miniapp/ads/MiniAppH5AdsProvider.kt
@@ -1,7 +1,2 @@
 package com.rakuten.tech.mobile.miniapp.ads
 
-import android.content.Context
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import com.google.android.gms.ads.h5.H5AdsWebViewClient
-
