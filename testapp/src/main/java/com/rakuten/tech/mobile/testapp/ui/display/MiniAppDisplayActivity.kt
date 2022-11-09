054320d2b06cb3c618455fc2759644f5d935d47d
Shohieb <shohiebsense@users.noreply.github.com>
2022-11-09 15:10:14 +0900

    fix[MINI-5634] remove remark

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/display/MiniAppDisplayActivity.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/display/MiniAppDisplayActivity.kt
@@ -265,7 +265,6 @@ class MiniAppDisplayActivity : BaseActivity(), PreloadMiniAppWindow.PreloadMiniA
                         this@MiniAppDisplayActivity, url,
                         appId, appUrl, externalWebViewReqCode
                     )
-                    //finish()
                 }
             }
         }
