efd87ab592112d7f075cabdb6abe6f836d045dfe
Shohieb <shohiebsense@users.noreply.github.com>
2022-12-08 17:38:04 +0900

    fix[MINI-5751] use the cached miniapview even after fragment destroyed

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/miniapptabs/fragments/MiniAppDisplayFragment.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/miniapptabs/fragments/MiniAppDisplayFragment.kt
@@ -127,8 +127,11 @@ class MiniAppDisplayFragment : BaseFragment(), PreloadMiniAppWindow.PreloadMiniA
     }
 
     private fun getMiniAppView(activity: Activity): MiniAppView {
-        if(activity is DemoAppMainActivity) {
-            miniAppIdAndViewMap.remove(Pair(activity.getCurrentSelectedId(), appId))
+        if (activity is DemoAppMainActivity) {
+            val keyPair = Pair(activity.getCurrentSelectedId(), appId)
+            if (miniAppIdAndViewMap.containsKey(keyPair)) {
+                miniAppIdAndViewMap.remove(keyPair)
+            }
         }
         return MiniAppView.init(createMiniAppInfoParam(activity, args.miniAppInfo))
     }
