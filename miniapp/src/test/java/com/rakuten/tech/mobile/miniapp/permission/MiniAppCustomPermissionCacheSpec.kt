b3d6d34b65acb47b9b71f55ab20bc4e93793ce94
munir <mdmunir.hossain@rakuten.com>
2023-01-13 13:43:05 +0900

    fixed imports

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/permission/MiniAppCustomPermissionCacheSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/permission/MiniAppCustomPermissionCacheSpec.kt
@@ -14,6 +14,7 @@ import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.spy
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import org.mockito.kotlin.never
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
