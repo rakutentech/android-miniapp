cb0aa8fa85777be241a69b740b4af148300f4fd7
munir <mdmunir.hossain@rakuten.com>
2023-07-05 17:04:16 +0900

    added demo app changes.

--- testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/QASettingsActivity.kt
+++ testapp/src/main/java/com/rakuten/tech/mobile/testapp/ui/userdata/QASettingsActivity.kt
@@ -18,7 +18,6 @@ import com.rakuten.tech.mobile.miniapp.errors.MiniAppAccessTokenError
 import com.rakuten.tech.mobile.miniapp.js.hostenvironment.HostAppThemeColors
 import com.rakuten.tech.mobile.miniapp.testapp.R
 import com.rakuten.tech.mobile.miniapp.testapp.databinding.QaSettingsActivityBinding
-import com.rakuten.tech.mobile.testapp.helper.FileUtils
 import com.rakuten.tech.mobile.testapp.helper.MiniAppBluetoothDelegate
 import com.rakuten.tech.mobile.testapp.helper.hideSoftKeyboard
 import com.rakuten.tech.mobile.testapp.helper.showToastMessage
@@ -127,7 +126,9 @@ class QASettingsActivity : BaseActivity() {
         binding.edtPrimaryColor.setText(settings.colorTheme.primaryColor)
         binding.edtSecondaryColor.setText(settings.colorTheme.secondaryColor)
 
-        binding.tvAnalyticsLogs.text = FileUtils.getAnalyticLogs(this)
+        binding.btnShowLogs.setOnClickListener {
+            QAAnalyticsLogsActivity.start(this)
+        }
 
         invalidateMaxStorageField()
     }
