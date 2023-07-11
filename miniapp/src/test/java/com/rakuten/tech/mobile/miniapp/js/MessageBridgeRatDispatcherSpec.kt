96968fcfa0763466fbc06a31937d03a796b49051
leojoseph.r <leojoseph.r@rakuten.com>
2023-07-11 12:51:14 +0900

    Testcase fix

--- miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MessageBridgeRatDispatcherSpec.kt
+++ miniapp/src/test/java/com/rakuten/tech/mobile/miniapp/js/MessageBridgeRatDispatcherSpec.kt
@@ -48,8 +48,6 @@ class MessageBridgeRatDispatcherSpec {
         ActionType.GET_HOST_APP_THEME_COLORS.action to Actype.GET_HOST_APP_THEME_COLORS,
         ActionType.GET_IS_DARK_MODE.action to Actype.GET_IS_DARK_MODE,
         ActionType.SEND_MA_ANALYTICS.action to Actype.SEND_MA_ANALYTICS,
-        ActionType.UNIVERSAL_BRIDGE_INFO to Actype.RECEIVE_UNIVERSAL_BRIDGE_INFO,
-
         )
 
     @Test
